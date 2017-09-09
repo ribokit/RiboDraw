60010e58e6cb52ab06589cf19e929367b9fc6e62
Rhiju Das <rhiju@stanford.edu>
2017-09-09 15:28:51 -0700

    trying for more elegant arrow shape

--- Scripts/draw_helix.m
+++ Scripts/draw_helix.m
@@ -262,13 +262,15 @@ setappdata( gca, res_tag, residue);
 function update_arrow( h, ctr, v, visible, spacing );
 x = v * [0 1; -1 0]; % cross direction
 set( h, 'visible', visible);
-a1 = ctr - spacing/2*v-spacing/5*x;
-a2 = ctr - spacing/2*v+spacing/5*x;
-a3 = ctr + spacing/2*v;
+a1 = ctr - spacing/3*v-spacing/5*x;
+a2 = ctr - spacing/3*v+spacing/5*x;
+a3 = ctr - spacing/6*v+spacing/10*x;
+a4 = ctr + spacing/2*v;
+a5 = ctr - spacing/6*v-spacing/10*x;
 set( h, 'xdata', ...
-    [a1(1) a2(1) a3(1)] );
+    [a1(1) a2(1) a3(1) a4(1) a5(1)] );
 set( h, 'ydata', ...
-    [a1(2) a2(2) a3(2)] );
+    [a1(2) a2(2) a3(2) a4(2) a5(2)] );
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -305,8 +307,10 @@ set( h, 'ydata', vertices(:,2) );
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function move_residue(h)
-% works for both text (residue) and rectangle.
-% Then we retrieve the current rectangle position
+% don't really need this function, but allows
+% snap to grid during movement.
+
+% works for both text (residue) and rectangle (helix).
 pos = get(h,'Position');
 if length( pos ) == 4 % rectangle
     res_center = [pos(1)+pos(3)/2, pos(2)+pos(4)/2];

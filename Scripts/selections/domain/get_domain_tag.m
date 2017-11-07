11b1dfe5d7fdbd60b321b84ecda65934cfcc9611
Rhiju Das <rhiju@stanford.edu>
2017-11-07 09:57:06 -0800

    update get_domain_tag to look for tag with _domain tail.

--- Scripts/selections/domain/get_domain_tag.m
+++ Scripts/selections/domain/get_domain_tag.m
@@ -1,6 +1,10 @@
 function tag = get_domain_tag( name );
 
-tag = sprintf('Selection_%s', strrep(name, ' ', '_' ) );
+tag = sprintf('Selection_%s_domain', strrep(name, ' ', '_' ) );
+if ~isappdata( gca, tag );
+    tag = sprintf('Selection_%s', strrep(name, ' ', '_' ) );
+end
+
 if ~isappdata( gca, tag );
     tag = name;
     if ~isappdata( gca, tag ) 

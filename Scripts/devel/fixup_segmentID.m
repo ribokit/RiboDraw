bd5298a5d8244df2c09ff852926e6492a9bc3da2
Rhiju Das <rhiju@stanford.edu>
2017-11-07 09:56:47 -0800

    remove unused devel script

--- Scripts/devel/fixup_segmentID.m
+++
@@ -1,9 +0,0 @@
-function replace_segid( segid, new_segid );
-
-res_tags = get_tags( 'Residue' );
-for i = 1:length( res_tags )
-    tag = res_tags{i};
-    obj = getappdata( gca, tag );
-    obj.segid = segid;
-    obj.res_tag = obj.res_tag(1:8)
-end

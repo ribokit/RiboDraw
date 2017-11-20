3baadcf69921b076220083671fd256c521eb9b57
Rhiju Das <rhiju@stanford.edu>
2017-11-20 15:25:17 -0800

    better doc for fade_color, and allow setting of fade_extent

--- scripts/color/fade_color.m
+++ scripts/color/fade_color.m
@@ -1,5 +1,8 @@
-function color = fade_color( color );
+function color = fade_color( color, fade_extent );
 % color = fade_color( color );
+%
 % make color whiter by 70%.
-fade_extent = 0.3;
+%
+% (C) R. Das, Stanford University, 2017
+if ~exist( 'fade_extent', 'var') fade_extent = 0.3; end;
 color = [1.0,1.0,1.0] - fade_extent * ( [1.0,1.0,1.0] - color );

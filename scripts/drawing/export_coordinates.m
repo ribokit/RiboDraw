2b4bf715d20f5191f83f74065488d8ea5e1493d1
Rhiju Das <rhiju@stanford.edu>
2018-04-15 13:12:29 -0700

    actually output scaled coordinates in export_coordinates

--- scripts/drawing/export_coordinates.m
+++ scripts/drawing/export_coordinates.m
@@ -33,7 +33,7 @@ for i = 1:length( res_tags )
     res = getappdata( gca, res_tags{i} );
     x = sf1 * ( res.plot_pos(1) - xl(1) );
     y = sf2 * ( yl(2) - res.plot_pos(2) ); % reverse MATLAB's y-axis since every other program does that.
-    outstring = sprintf( '%7.3f\t%7.3f\t%s\t%s\t%d\t%s\n',res.plot_pos(1),res.plot_pos(2),res.chain,res.segid,res.resnum,strrep(res.nucleotide,'\','\\'));
+    outstring = sprintf( '%7.3f\t%7.3f\t%s\t%s\t%d\t%s\n',x,y,res.chain,res.segid,res.resnum,strrep(res.nucleotide,'\','\\'));
     %fprintf( outstring );
     fprintf( fid, outstring );
     coords(i,:) = [x,y];

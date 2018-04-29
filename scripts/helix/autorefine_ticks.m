fd7070d9db78bedc539cf3250e9b4ee6b3a961c3
Rhiju Das <rhiju@stanford.edu>
2018-04-28 21:56:18 -0700

    slight speedup to autorefine_ticks

---
+++ scripts/helix/autorefine_ticks.m
@@ -0,0 +1,45 @@
+function [scores,sample_tickrot] = autorefine_tick( res_tags )
+% autorefine_tick( res_tag )
+sample_tickrot = [0:45:315];
+scores = [];
+if ~exist( 'res_tags','var' ); res_tags  = get_tags( 'Residue_' ); end;
+if ischar( res_tags ) res_tags = {res_tags}; end;
+
+axlim = axis();
+plot_settings = getappdata( gca, 'plot_settings' );
+fontsize = plot_settings.fontsize;
+
+for i = 1:length( res_tags );
+    res_tag = res_tags{i};
+    residue = getappdata( gca, res_tag );
+    if isfield( residue, 'tickrot' )
+        fprintf( 'Refining: %s\n', res_tag );
+        original_tickrot = residue.tickrot;
+        
+        plot_pos = residue.plot_pos;
+        axis( [plot_pos(1)-50, plot_pos(1)+50,plot_pos(2)-50, plot_pos(2)+50] );
+        reset_fontsize();
+        plot_settings = getappdata( gca, 'plot_settings' );
+        helix = getappdata( gca, residue.helix_tag );
+
+        R = get_helix_rotation_matrix( helix );
+        for j = 1:length( sample_tickrot )
+            tickrot = sample_tickrot(j);
+            residue.tickrot = tickrot;
+            setappdata( gca, residue.res_tag, residue );
+            update_tick( residue, plot_settings, R);
+            %cdata = print('-RGBImage','-r50');
+            cdata = screencapture(gca);
+            scores(j) = sum(sum(sum(cdata)));
+        end
+        
+        [~,idx] = min( scores );
+        residue.tickrot = sample_tickrot( idx );
+        update_tick( residue, plot_settings, R);
+        setappdata( gca, residue.res_tag, residue );
+        cdata = screencapture(gca);
+    end
+end
+    
+axis( axlim );
+reset_fontsize( fontsize );

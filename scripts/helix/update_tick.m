b8daff2b226d2a682a5f4e7eec06777516c8cda4
Rhiju Das <rhiju@stanford.edu>
2018-04-30 21:55:07 -0700

    nudge out tick label when connecting to longer name nucleotide

--- scripts/helix/update_tick.m
+++ scripts/helix/update_tick.m
@@ -6,11 +6,16 @@ fontsize = plot_settings.fontsize;
 
 theta = residue.tickrot;
 v = [cos(theta*pi/180), sin(theta*pi/180)]*R;
-tickpos1 = residue.plot_pos + v*bp_spacing/3;
-tickpos2 = residue.plot_pos + v*bp_spacing*2/3;
+nudge  =  bp_spacing/3;
+if length( residue.nucleotide ) >= 3 && mod(theta,180) == 90
+    nudge = nudge + ( length( residue.nucleotide ) - 2 ) * bp_spacing/10;
+end
+nudge2 =  nudge + bp_spacing/3;
+tickpos1 = residue.plot_pos + v*nudge;
+tickpos2 = residue.plot_pos + v*nudge2;
 set( residue.tick_handle, 'xdata', [tickpos1(1) tickpos2(1)] );
 set( residue.tick_handle, 'ydata', [tickpos1(2) tickpos2(2)] );
-labelpos = residue.plot_pos + v*bp_spacing*2/3;
+labelpos = residue.plot_pos + v*nudge2;
 set( residue.tick_label, 'position', labelpos );
 plot_settings = getappdata( gca, 'plot_settings' );
 if ( get(residue.tick_label, 'fontsize') ~= plot_settings.fontsize )

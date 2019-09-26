97ce6550bff46708b93ea33e78229f25acc67b48
Rhiju Das <rhiju@stanford.edu>
2019-09-26 09:38:28 -0700

    udpate set_eterna_theme so fontsize and rings look closer to on-screen graphics

--- scripts/residue/color_fill_circles_eterna.m
+++ scripts/residue/color_fill_circles_eterna.m
@@ -1,4 +1,4 @@
-function color_fill_circles_by_residue_type()
+function color_fill_circles_eterna()
 % color_undercircles_by_residue_type()
 %
 % Create undercircles under A,C,G,U and color by eterna colors
@@ -19,7 +19,8 @@ for i = 1:length( res_tags )
         case 'U'
             residue.fill_color = hex2rgb('#3193D1');
     end            
+    residue.ring_color = [0.7 0.7 0.7];
     setappdata( gca, res_tags{i}, residue );
     if isfield(residue,'fill_color' ); draw_fill_and_ring_circle( residue, plot_settings ); end;
 end
-show_fill_circles();
+show_fill_and_ring_circles();
--- scripts/settings/set_eterna_theme.m
+++ scripts/settings/set_eterna_theme.m
@@ -1,4 +1,4 @@
-function setup_eterna_theme();
+function set_eterna_theme();
 %
 % for fun, visualize how ribodraw might look like if it
 %  is displayed in Eterna.
@@ -9,6 +9,16 @@ function setup_eterna_theme();
 % (C) R. Das, Stanford University, 2019
 
 set_bg_color( [16,33,59]/255 );
+color_drawing( 'black' );
 set_line_color( 'white' );
 set_symbol_color( 'white' );
+color_fill_circles_eterna
+
+plot_settings = get_plot_settings();
+plot_settings.bp_spacing = plot_settings.spacing;
+plot_settings.eterna_theme = 1;
+plot_settings.font_size = 12;
+setappdata( gca, 'plot_settings', plot_settings );
+
 fprintf( '\n Type set_default_theme() to restore the usual colors.\n' )
+set_artboards

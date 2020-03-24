d2e0ebe533bdec2ede8640ff42ca3d0a0d5d336c
Andy Watkins <andy.watkins2@gmail.com>
2020-03-23 20:01:05 -0400

    Add set_fontcolor function

--- scripts/settings/set_boldface.m
+++ scripts/settings/set_boldface.m
@@ -1,4 +1,4 @@
-function set_boldface( setting )
+function set_boldface( setting, res_tags )
 % set_boldface( setting )
 %
 % Set name labels to be boldface or not, depending on setting.
@@ -6,7 +6,12 @@ function set_boldface( setting )
 % (C) R. Das, Stanford University, 2017
 
 if ~exist( 'setting', 'var' ) setting = 1; end;
-res_tags = get_tags( 'Residue' );
+
+% if you don't pass res_tags, get all residues.
+if nargin < 2
+    res_tags = get_tags( 'Residue' );
+end;
+
 if setting == 1; fontweight = 'bold'; else; fontweight = 'normal'; end;
 
 for i = 1:length( res_tags )
@@ -16,6 +21,9 @@ for i = 1:length( res_tags )
     end
 end
 
-plot_settings = getappdata( gca, 'plot_settings' );
-plot_settings.boldface = setting;
-setappdata( gca, 'plot_settings', plot_settings );
+% Only change the global plot_settings if res_tags not specified
+if nargin < 2
+    plot_settings = getappdata( gca, 'plot_settings' );
+    plot_settings.boldface = setting;
+    setappdata( gca, 'plot_settings', plot_settings );
+end;

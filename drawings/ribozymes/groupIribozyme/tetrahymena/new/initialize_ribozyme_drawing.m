b221024a0e9fb260697afb65b10cd407247de18c
Rhiju Das <rhiju@stanford.edu>
2020-05-03 12:30:26 -0700

    get ready for tetrahymena ribozyme new structure (ribosolve)

---
+++ drawings/ribozymes/groupIribozyme/tetrahymena/new/initialize_ribozyme_drawing.m
@@ -0,0 +1,25 @@
+%initialize_drawing( 'rna_motif/ribozyme.pdb' );
+
+hide_helix_label( 'P4*')
+hide_helix_label( 'P5*')
+hide_helix_label( 'P5a*')
+hide_helix_label( 'P5a**')
+hide_helix_label( 'P9.2*')
+hide_helix_label( 'P2.1*')
+color_drawing('black');
+color_drawing( 'marine', 'N:127-195');
+color_drawing( 'teal', 'N:262-268 N:307-312');
+color_drawing( 'orange', 'N:116-121 N:200-205');
+color_drawing( 'wheat', 'N:96-102 N:272-278');
+color_drawing( 'forest', 'N:215-258');
+color_drawing( 'pink','N:107-112 N:208-214');
+color_drawing( 'slate','N:369-402');
+color_drawing( 'density','N:31-56');
+color_drawing( 'purple','N:279-299');
+color_drawing( 'red','N:318-331');
+color_drawing( 'yellow','N:315-316 N:405-406');
+color_drawing( 'ruby','N:58-93');
+color_drawing( 'green','N:332-367');
+
+
+
